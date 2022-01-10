81228df776dc8b9523b7e14adc1d773bb8d25418
always-reach <67514209+always-reach@users.noreply.github.com>
2022-01-10 11:20:49 +0900

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: process.cwd(),
   plugins: [react()],
+  build:{
+    outDir:"./build"
+  },
   server:{
     host:'0.0.0.0'
   }
