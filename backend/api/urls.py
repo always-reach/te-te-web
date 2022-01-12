66f2b4ec7b2a798c328194565d95bed3448da05a
always-reach <67514209+always-reach@users.noreply.github.com>
2022-01-12 23:06:41 +0900

    delete token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TestAPI, LikedTweetAPI
+from .views import TestAPI, LikedTweetAPI, CreateAuthenticateEndpoint
 
 urlpatterns = [
     path(r"test", TestAPI.as_view(), name="detail"),
+    path(r"auth_twitter",CreateAuthenticateEndpoint.as_view()),
     path(r"liked_tweet", LikedTweetAPI.as_view())
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 import requests
+from requests_oauthlib import OAuth2Session
 
 # Create your views here.
 from rest_framework.response import Response
@@ -12,11 +13,18 @@ class TestAPI(APIView):
         return Response({"test": "get"})
 
 
+class CreateAuthenticateEndpoint(APIView):
+    def get(self, request, *args, **kwargs) -> Response:
+        twitter = OAuth2Session(client_id=CLIENT_ID)
+        authorization_url, state = twitter.authorization_url()
+        print(authorization_url, state)
+        return Response({"test": "hoge"})
+
+
 class LikedTweetAPI(APIView):
     def get(self, request, *args, **kwargs) -> Response:
-        user_id = "1365833961918722048"
-        bearer_token: string = "AAAAAAAAAAAAAAAAAAAAADX3WQEAAAAAs2gl8fN4BgszWpN0l0wsaj2%2B19M%3D2kbvsTN7DDdRTAe2dlLnqDPGpNhBhZpzeC0kocaustllURW1cg"
-        url: string = "https://api.twitter.com/2/users/{}/liked_tweets".format(user_id)
+        oauth = OAuth2Session()
+        url: str = "https://api.twitter.com/2/users/{}/liked_tweets".format(user_id)
         header: dict = {"Authorization": "Beare {}".format(bearer_token)}
 
         response: requests.models.Response = requests.get(url=url, headers=header)
