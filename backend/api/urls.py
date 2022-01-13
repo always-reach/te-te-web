d4775fef98d0f4ad468330c67e64948c745126a5
always-reach <67514209+always-reach@users.noreply.github.com>
2022-01-14 00:12:54 +0900

    get liked tweet

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from .views import TestAPI, LikedTweetAPI, CreateAuthenticateEndpoint
+from .views import LikedTweetAPI
 
 urlpatterns = [
-    path(r"test", TestAPI.as_view(), name="detail"),
-    path(r"auth_twitter",CreateAuthenticateEndpoint.as_view()),
     path(r"liked_tweet", LikedTweetAPI.as_view())
 ]
