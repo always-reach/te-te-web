9f5d0b576ce4156fa65a1fb43a4a7acf4743a536
always-reach <67514209+always-reach@users.noreply.github.com>
2022-01-23 15:43:30 +0900

    formant response and display images

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import LikedTweetAPI
 
 urlpatterns = [
-    path(r"liked_tweet", LikedTweetAPI.as_view())
+    path(r"liked_tweet", LikedTweetAPI.as_view()),
 ]
--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,51 @@
 import * as React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import { SearchLikedTweet } from './api/Twitter'
+
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+
+import {FetchLiekedTweets,Tweet} from '../types/index'
+import tweetInit from './test.json'
+
 
 function App() {
-  const [count, setCount] = React.useState(0)
+  const [likedTweets,setlikedTweets]=React.useState<FetchLiekedTweets|null>(null)
 
   React.useEffect(()=>{
-    const getLiked=async()=>{
+    /* const getLiked=async()=>{
       const respone=await SearchLikedTweet("000")
       const responeJson=await respone.json()
       console.log("liked tweets",responeJson)
-    }
+    } */
 
     //getLiked()
+    setlikedTweets(tweetInit as FetchLiekedTweets)
   },[])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Box>
+      {likedTweets?.data.map((tweetElements:Tweet)=>{
+        return(
+        <Card key={tweetElements.text} variant="outlined">
+          <CardContent>
+            <Typography>{tweetElements.text}</Typography>
+            {tweetElements.media?.map((url)=>{
+                return(
+                    <CardMedia
+                    key={url}
+                    component="img"
+                    image={url}
+                    alt="twitter_image"
+                    />
+                )
+            })}
+          </CardContent>
+        </Card>)
+      })}
+    </Box>
   )
 }
 
